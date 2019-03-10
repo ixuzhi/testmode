a62c05a76635395a47702645b8c78db7a3eee4ae
ixuzhi <ixuzhi100@gmail.com>
2019-03-10 15:35:40 +0000

    fix for v2

--- usetestmode/go.mod
+++ usetestmode/go.mod
@@ -1,3 +1,3 @@
-module github.com/ixuzhi/testmod/v2
+module github.com/ixuzhi/testmode/v2
 
 go 1.12
--- usetestmode/main.go
+++ usetestmode/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/ixuzhi/testmod/v2"
+	"github.com/ixuzhi/testmode/v2"
 )
 
 func main() {

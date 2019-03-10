0ab28b93a91ec676d7bbac878edc608941087b15
ixuzhi <ixuzhi100@gmail.com>
2019-03-10 16:11:59 +0000

    fix usertestmode v1/v2.

--- usetestmode/go.mod
+++ usetestmode/go.mod
@@ -1,3 +1,7 @@
-module github.com/ixuzhi/testmode/v2
+module mod
+
+require github.com/ixuzhi/testmode v1.0.0
+
+require github.com/ixuzhi/testmode/v2 v2.0.0
 
 go 1.12
--- usetestmode/main.go
+++ usetestmode/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/ixuzhi/testmode/v2"
+	"github.com/ixuzhi/testmode"
+	testmode_v2 "github.com/ixuzhi/testmode/v2"
 )
 
 func main() {
-	fmt.Println(testmode.Hi("user testmode case!","ch"))
+	fmt.Println(testmode.Hi("user testmode case!"))
+	fmt.Println(testmode_v2.Hi("user testmode case!","ch"))
 }

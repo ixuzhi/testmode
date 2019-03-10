beb2008a9f69e75173002003e1d778574ba4c9f4
ixuzhi <ixuzhi100@gmail.com>
2019-03-10 12:45:57 +0000

    tagv1.0.0

--- testmode.go
+++ testmode.go
@@ -3,6 +3,6 @@ package testmode
 import "fmt"
 
 func Hi(name string) string {
-	return fmt.Sprintf("hi,testmode:%s", name)
+	return fmt.Sprintf("hi,testmode:%s,%s", name,version())
 
 }
---
+++ version.go
@@ -0,0 +1,7 @@
+package testmode
+
+import "fmt"
+
+func version()string{
+	return fmt.Sprintf("version:%s","tag1.0.0")
+}
